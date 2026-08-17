19e8f090079af429791cf66922fa501aaa0d089e
YASH BARAIYA <yashbaraiyawork3011@gmail.com>
2026-08-18 02:24:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Middle of Three/middle-of-three.java
@@ -0,0 +1,11 @@
+class Solution {
+	int middle(int a, int b, int c) {
+		// code here
+		if (a>b && a<c || a<b && a>c)
+			return a;
+		else if (b>a && b<c || b<a && b>c)
+			return b;
+		else
+			return c;
+	}
+}
